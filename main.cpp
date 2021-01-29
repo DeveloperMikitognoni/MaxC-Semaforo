0787ab59f3e1f9307dfd1acd84cec1ebbe455b77
Devmiki <miki@devmiki.tk>
2021-01-29 17:24:48 +0000

    Inserite frasi UI

--- main.cpp
+++ main.cpp
@@ -3,24 +3,24 @@ using namespace std;
 
 int main() {
   float n;
-  cout << "Inserire due numeri da 0 a 2" << endl;
+  cout << "Inserire un numero tra 0 a 2" << endl;
   int i = 12;
 do{
   cin >> n;
 	if (n > 2 || n < 0)
   {
-	cout << "Numero 0 2" << endl;
+	cout << "Il numero deve essere compreso tra 0 e 2" << endl;
 	}
 	if(n == 0) {
-	cout << "rosso" << endl;
+	cout << "Il semaforo è rosso" << endl;
 	i=11;
 	}
 	else if(n == 1) {
-	cout << "giallo" << endl;
+	cout << "Il semaforo è giallo" << endl;
 	i=11;
 	}
 	else if(n == 2) {
-	cout << "verde" << endl;
+	cout << "Il semaforo è verde" << endl;
 	i=11;
 	}
 } while( i == 12);
